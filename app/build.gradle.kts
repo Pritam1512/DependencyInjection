3c7b6371407900bdd4fccc19d1029a36e41a6562
pritam.kumar <pritam.kumar@samsung.com>
2024-11-21 12:00:57 +0530

    Field injection using @Inject and inject(activity) method inside components

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,6 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("com.google.dagger:dagger:2.28.3")
-    kapt("com.google.dagger:dagger-compiler:2.28.3")
+    implementation("com.google.dagger:dagger:2.52")
+    kapt("com.google.dagger:dagger-compiler:2.52")
 }
--- app/src/main/java/com/example/batteryintent/MainActivity.kt
+++ app/src/main/java/com/example/batteryintent/MainActivity.kt
@@ -3,13 +3,26 @@ package com.example.batteryintent
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var userRegistrationService: UserRegistrationService
+
+    @Inject
+    lateinit var emailService: EmailService
+
+    @Inject
+    lateinit var saveUserToDB: SaveUserToDB
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val userRegistrationService = DaggerUserRegistrationComponent.builder().build()
-        userRegistrationService.getUserRegistrationService().registerUser("pritam@gmail.com","12345")
+        val userRegistrationComponent = DaggerUserRegistrationComponent.builder().build()
+        userRegistrationComponent.inject(this)
+        userRegistrationService.registerUser("pritam@gmail.com", "12345")
+
     }
 }
